1c986dac426f0580ae3b2ee2d3ac4c6054aad1d1
stepan <jomolun@yandex.com>
2024-03-23 18:21:23 +0300

    fix

--- app/routes.py
+++ app/routes.py
@@ -107,6 +107,7 @@ def start_recording():
         if not meeting_url:
             return abort(400, description="Meeting URL is required")
         
+        logger.info(f"get req: {request}")
         resp = recall_api.start_recording("Kotegov Dmitry", CONFIG["DESTINATION_URL"])
 
         logger.info(f"/start_recording: {resp.json()}")
