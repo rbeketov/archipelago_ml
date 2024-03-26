fc651a8529996111f48438d06afe9653cc2a24d6
stepan <jomolun@yandex.com>
2024-03-26 22:37:07 +0300

    deploy

---
+++ deploy/daemon.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# should be executed in py venv
+# should be executed in with sudo
+
+${PROJECT_DIR:-/home/ubuntu/archipelago_ml}
+
+pip install j2cli
+
+export python_interpreter=$(which python)
+export project_dir=$PROJECT_DIR
+j2 ./deploy/ml.service.j2 --import-env -o /etc/systemd/system/ml.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable ml.service
