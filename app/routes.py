3dc72035ab31d52d817c08a637b71c32ce74c861
stepan <jomolun@yandex.com>
2024-03-27 01:33:49 +0300

    fix

--- app/routes.py
+++ app/routes.py
@@ -305,7 +305,7 @@ def get_zoom_sum():
             MODEL_URI_SUMM,
             SystemPromts.SUMMARAIZE,
             API_KEY,
-            0.1
+            1.0
         )
 
         logger.info(f"Суммаризированный текст {summ_text}")
