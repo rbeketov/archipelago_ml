1b601bfc4bbab41820e92db3b5143f1d457dc111
stepan <jomolun@yandex.com>
2024-05-02 17:53:43 +0300

    token removed

--- app/http/meeting_bot.py
+++ app/http/meeting_bot.py
@@ -22,7 +22,6 @@ def make_bot_handler(config: Config, bot_net: BotNet) -> Blueprint:
         class RequestFields(StrEnum):
             USER_ID = "user_id"
             MEETING_URL = "url"
-            TOKEN_VALUE = "token"
             SUMMARY_DETAIL = "summary_detail"
 
         with HttpException400(logger=logger):
@@ -30,12 +29,9 @@ def make_bot_handler(config: Config, bot_net: BotNet) -> Blueprint:
             if not request.is_json:
                 return json_error(400, description="Request body must be JSON")
 
-            # validate
-            token = request.json[RequestFields.TOKEN_VALUE]
-            if token != config.env.TOKEN:
-                return json_error(403)
-
-            detalization = request.get_json().get(RequestFields.SUMMARY_DETAIL, "Средняя")
+            detalization = request.get_json().get(
+                RequestFields.SUMMARY_DETAIL, "Средняя"
+            )
             summary_detail_prompt = config.prompts.SUMMARAIZE_WITH_DETAIL(detalization)
 
             meeting_url = request.get_json().get(RequestFields.MEETING_URL)
@@ -74,17 +70,11 @@ def make_bot_handler(config: Config, bot_net: BotNet) -> Blueprint:
     def stop_recording():
         class RequestFields(StrEnum):
             SUMM_ID = "summ_id"
-            TOKEN_VALUE = "token"
 
         with HttpException400(logger=logger):
             logger.info(f"get req: {request.json}")
             if not request.is_json:
                 return json_error(400, description="Request body must be JSON")
-
-            # validate
-            token = request.json[RequestFields.TOKEN_VALUE]
-            if token != config.env.TOKEN:
-                return json_error(403)
 
             bot_id = request.get_json().get(RequestFields.SUMM_ID)
             if not bot_id:
@@ -103,17 +93,11 @@ def make_bot_handler(config: Config, bot_net: BotNet) -> Blueprint:
     def bot_state():
         class RequestFields(StrEnum):
             SUMM_ID = "summ_id"
-            TOKEN_VALUE = "token"
 
         with HttpException400(logger=logger):
             logger.info(f"get req: {request.json}")
             if not request.is_json:
                 return json_error(400, description="Request body must be JSON")
-
-            # validate
-            token = request.json[RequestFields.TOKEN_VALUE]
-            if token != config.env.TOKEN:
-                return json_error(403)
 
             bot_id = request.get_json().get(RequestFields.SUMM_ID)
             if not bot_id:
@@ -136,7 +120,7 @@ def make_bot_handler(config: Config, bot_net: BotNet) -> Blueprint:
             bot_id = payload["bot_id"]
             transcript = payload["transcript"]
 
-            from ..meeting_bots.bot import Transcription # maybe cyclic
+            from ..meeting_bots.bot import Transcription  # maybe cyclic
 
             bot = bot_net.get_by_bot_id(bot_id=bot_id)
             if bot is not None:
@@ -181,18 +165,12 @@ def make_bot_handler(config: Config, bot_net: BotNet) -> Blueprint:
         class RequestFields(StrEnum):
             # USER_ID = "user_id"
             SUMM_ID = "summ_id"
-            TOKEN_VALUE = "token"
             ROLE = "role"
 
         with HttpException400(logger=logger):
             logger.info(f"get req: {request.json}")
             if not request.is_json:
                 return json_error(400, description="Request body must be JSON")
-
-            # validate
-            token = request.json[RequestFields.TOKEN_VALUE]
-            if token != config.env.TOKEN:
-                return json_error(403)
 
             bot_id = request.get_json().get(RequestFields.SUMM_ID)
             if not bot_id:
@@ -205,8 +183,14 @@ def make_bot_handler(config: Config, bot_net: BotNet) -> Blueprint:
             logger.info(f"bot net: {bot_net}")
 
             # TODO: make sure bot existed
-            summ_with_role, summ_role, active = none_unpack(bot_net.summary_repo.get_summ_with_role(bot_id=bot_id), 3)
-            if summ_with_role is not None and summ_role == role and summ_with_role != "":
+            summ_with_role, summ_role, active = none_unpack(
+                bot_net.summary_repo.get_summ_with_role(bot_id=bot_id), 3
+            )
+            if (
+                summ_with_role is not None
+                and summ_role == role
+                and summ_with_role != ""
+            ):
                 json_data = {"summ_text": summ_with_role, "has_sum": True}
 
             summ, active = none_unpack(bot_net.summary_repo.get_summ(bot_id=bot_id), 2)
@@ -228,7 +212,9 @@ def make_bot_handler(config: Config, bot_net: BotNet) -> Blueprint:
             # make async (after response)
             # dont dublicate input if role is default
             # handle update_res == False
-            update_res = bot_net.summary_repo.update_role_text(bot_id=bot_id, summary_with_role=summ, role=role)
+            update_res = bot_net.summary_repo.update_role_text(
+                bot_id=bot_id, summary_with_role=summ, role=role
+            )
 
             json_data = {"summ_text": summ, "has_sum": True}
             return jsonify(json_data)
